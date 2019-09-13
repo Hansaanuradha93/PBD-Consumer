8ea6a23d3d77511ba231e9b413b2019e98e90fe2
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-09-13 23:10:27 +0530

    Added new functionality to get multiple inputs and display the prime factors for each input number

--- app/src/main/java/com/example/hansaanuradha/consumer/MainActivity.kt
+++ app/src/main/java/com/example/hansaanuradha/consumer/MainActivity.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import java.util.*
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,25 +19,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Views for one number input
         val sendButton = findViewById<Button>(R.id.sendButton)
         val inputText = findViewById<EditText>(R.id.wholeNumberEditText)
-//        val resultText = findViewById<TextView>(R.id.resultTextView)
 
-        // Onclick listener
+        // Views for multiple numbers input
+        val comaSeparatedEditText = findViewById<EditText>(R.id.comaSeparatedWholeNumbersEditText)
+        val sendNumbersButton = findViewById<Button>(R.id.sendNumbersButton)
+
+        // Onclick listeners
         sendButton.setOnClickListener {
-            Log.d("hello", "hello")
-            Log.v("EditText", inputText.getText().toString())
 
-            val sendIntent = Intent()
-            sendIntent.action = Intent.ACTION_SEND
-            sendIntent.putExtra(Intent.EXTRA_TEXT, inputText.getText().toString())
-            sendIntent.type = "text/plain"
-            startActivity(Intent.createChooser(sendIntent, resources.getText(R.string.send_to)))
+            val numberString = inputText.getText().toString()
 
+            if (!numberString.isNullOrEmpty()) {
+                val sendIntent = Intent()
+                sendIntent.action = Intent.ACTION_SEND
+                sendIntent.putExtra("SingleNumber", numberString)
+                sendIntent.type = "text/plain"
+                startActivity(Intent.createChooser(sendIntent, resources.getText(R.string.send_to)))
+            } else {
+                Toast.makeText(this, "Text Field is Empty", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        sendNumbersButton.setOnClickListener {
+            Log.v("Numbers", comaSeparatedEditText.getText().toString())
+
+            val numbersString = comaSeparatedEditText.getText().toString()
+
+            if (!numbersString.isNullOrEmpty()) {
+
+                val sendIntent = Intent()
+                sendIntent.action = Intent.ACTION_SEND
+                sendIntent.putExtra("MultipleNumbers", numbersString)
+                sendIntent.type = "text/plain"
+                startActivity(Intent.createChooser(sendIntent, resources.getText(R.string.send_to)))
+            } else {
+                Toast.makeText(this, "Text Field is Empty", Toast.LENGTH_SHORT).show()
+            }
 
         }
 
 
+        // TODO: - Pass this as a whole string to Producer, then split it, Get the result and convert it into a String in the Producer, Then send it back to the Consumer
 
     }
 
